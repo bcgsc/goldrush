b2ddc97db5437b6878a5147a0207283b211cf78f
vlad0x00 <vladlich@protonmail.com>
2021-12-22 16:31:30 -0800

    Clean the repo; move to meson

--- Workpackage2/btllib/docs/search/all_a.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['read_0',['read',['../classbtllib_1_1SeqReader.html#a7e8acc7bfbd8624cf8d9745922e9a034',1,'btllib::SeqReader']]],
-  ['read_5fblock_1',['read_block',['../classbtllib_1_1SeqReader.html#af8b9b6b1834f2fa6ac0bde20db0324a3',1,'btllib::SeqReader']]],
-  ['record_2',['Record',['../structbtllib_1_1Indexlr_1_1Record.html',1,'btllib::Indexlr::Record'],['../structbtllib_1_1SeqReader_1_1Record.html',1,'btllib::SeqReader::Record']]],
-  ['reverse_5fcomplement_3',['reverse_complement',['../namespacebtllib.html#a0c9cb0705283e0519330f79629cdc738',1,'btllib']]],
-  ['roll_4',['roll',['../classbtllib_1_1NtHash.html#a244c0c11655b6e1c52dd7c4528b8ed05',1,'btllib::NtHash::roll()'],['../classbtllib_1_1SeedNtHash.html#aa817b8acaf7c57d7be8ce47fcf5ac758',1,'btllib::SeedNtHash::roll()']]]
-];
