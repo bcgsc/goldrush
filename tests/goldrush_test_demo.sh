65b8028dbe72df8fc0365c5ff7fcc09faaf4ee8a
Lauren Coombe <lauren.e.coombe@gmail.com>
2022-09-23 15:52:17 -0700

    Add test data for checking GoldRush installation (#75)
    * Add goldrush test demo
    
    * Update test demo, add to yml
    
    * Update azure with path to installation
    
    * Test debug
    
    * Clean-up debug statements, add dependencies
    
    * Update compile
    
    * Update conda install
    
    * Add missing dependencies
    
    * Update python version for conda, add -B option to demo script
    
    * Add miller dependency, check L50 of assembly
    
    * Update README.md

---
+++ tests/goldrush_test_demo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+# Download the test data
+curl -L --output test_reads.fq https://www.bcgsc.ca/downloads/btl/goldrush/test/test_reads.fq
+
+# Run this demo to test your GoldRush installation
+echo "Launching GoldRush"
+goldrush run reads=test_reads G=1e6 t=4 p=goldrush_test -B
+
+l50=$(abyss-fac goldrush_test_golden_path.goldrush-edit-polished.span2.dist500.tigmint.fa.k40.w250.ntLink-5rounds.fa |mlr --tsv cut -f L50 |tail -n1)
+
+if [ -e goldrush_test_golden_path.goldrush-edit-polished.span2.dist500.tigmint.fa.k40.w250.ntLink-5rounds.fa ] && [ ${l50} -eq 1 ]; then
+  echo -e "\nTest successful!"
+else
+  echo -e "\nTest failed - please check your installation"
+  exit 1
+fi
+
+exit 0
