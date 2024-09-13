982ff47d2f7b09e77eac1c83edd855f6b342e1e4
Lauren Coombe <lauren.e.coombe@gmail.com>
2024-09-13 12:34:32 -0700

    Update goldpolish (#139)
    * Update goldpolish
    
    * Add includes

--- goldrush_path/calc_phred_average.hpp
+++ goldrush_path/calc_phred_average.hpp
@@ -1,6 +1,7 @@
 #ifndef CALC_PHRED_AVERAGE_H_
 #define CALC_PHRED_AVERAGE_H_
 #include <string>
+#include <cstdint>
 
 std::pair<uint32_t, uint32_t>
 calc_phred_average(const std::string qual);
--- goldrush_path/opt.hpp
+++ goldrush_path/opt.hpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <getopt.h>
 #include <string>
+#include <cstdint>
 
 namespace opt {
 
