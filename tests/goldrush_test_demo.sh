406b5e5c5a5c486ab2b57e77e3b0d8d5348d2d87
jowong04 <jowong@hpce704.hpc.bcgsc.ca>
2024-11-15 16:53:10 -0800

    adjust test case

--- tests/goldrush_test_demo.sh
+++ tests/goldrush_test_demo.sh
@@ -7,7 +7,7 @@ curl -L --output test_reads.fq https://www.bcgsc.ca/downloads/btl/goldrush/test/
 
 # Run this demo to test your GoldRush installation
 echo "Launching GoldRush"
-goldrush run reads=test_reads G=1e6 t=4 p=goldrush_test  -B
+goldrush run reads=test_reads G=1e6 t=4 p=goldrush_test P=0 -B
 
 l50=$(abyss-fac goldrush_test_golden_path.goldpolish-polished.span2.dist500.tigmint.fa.k40.w250.ntLink-5rounds.polished.fa |awk '{print $3}' |tail -n1)
 
